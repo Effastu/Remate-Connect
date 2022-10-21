3c3c8cac84f2654d61ca9059365df97a2f4c79de
carlosalvarez288963 <116127703+carlosalvarez288963@users.noreply.github.com>
2022-10-21 08:32:45 -0500

    Se crea formulario registro
    Se crea formulario de registro.
    Se adicionan en la pag de inicio producto manualmente, de esta forma se debe ver al terminar el proyecto.

--- archivos js/remate.js
+++ archivos js/remate.js
@@ -1,15 +1,49 @@
 
-let linkIniciarSesion = document.querySelector('#li-iniciar-sesion')
-let btnSalir = document.querySelector('#btn-salir')
-let contLogin = document.querySelector('.contenedor-login');
-let contHeader = document.querySelector('header')
+/* 1-Código para logear y btn salir del login*/
+
+let linkIniciarSesion = document.querySelector('#li-iniciar-sesion');
+let btnSalir = document.querySelector('#btn-salir-log');
+let contLogin = document.querySelector('#cont-login');
+let contHeader = document.querySelector('.encabezado');
+let contTargetas = document.querySelector('#cont-tar')
+
+/*El evento sobre el link iniciar sesión muestra el login y oculta lo demas*/
+
 linkIniciarSesion.addEventListener('click', (e) => {
     e.preventDefault();
-    contLogin.classList.add('mostrar');
-    contHeader.style.display = 'none'
+    contLogin.classList.add('mostrar-login');
+    contHeader.style.display = 'none';
+    contTargetas.style.display = 'none';
 });
+/*El evento sobre el btn salir cierrar el login y lleva a la pag inicio*/
+
 btnSalir.addEventListener('click', (e) => {
     e.preventDefault();
-    contLogin.classList.remove('mostrar');
-    contHeader.style.display = 'flex'
+    contLogin.classList.remove('mostrar-login');
+    contHeader.style.display = 'flex';
+    contTargetas.style.display = 'flex';
+
+})
+
+/* 2- Código para registrar y btn para salir modar registrar */
+
+let linkRegistrarUsuario = document.querySelector('#li-crear-cuenta');
+let btnSalirReg = document.querySelector('#btn-salir-reg');
+let contRegistrarse = document.querySelector('#cont-reg');
+
+linkRegistrarUsuario.addEventListener('click', (e)=>{
+    e.preventDefault();
+    contRegistrarse.classList.add('mostrar-reg');
+    contHeader.style.display = 'none';
+    contTargetas.style.display = 'none';
+
+}
+)
+
+btnSalirReg.addEventListener('click', (e)=>{
+    e.preventDefault();
+    contRegistrarse.remove('mostrar-reg')
+    contHeader.style.display = 'flex';
+    contTargetas.style.display = 'flex';
+
 })
