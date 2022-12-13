e8873c2253bcd59492805d33ecdb250dd458906c
Effastu <115671894+Effastu@users.noreply.github.com>
2022-12-12 19:56:22 -0500

    Update script_bdremates.sql

--- BaseDeDatos/script_bdremates.sql
+++ BaseDeDatos/script_bdremates.sql
@@ -1,75 +1,198 @@
-CREATE DATABASE BDremates;
-USE  BDremates;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 12-12-2022 a las 21:34:56
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.26
 
-CREATE TABLE cliente(
-idCliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nombreCliente VARCHAR(60) NOT NULL,
-apellidoCliente VARCHAR(60) NOT NULL,
-cedulaCliente VARCHAR(12) NOT NULL,
-contrasenaCliente VARCHAR(20) NOT NULL,
-nombreUsuario VARCHAR(40) NOT NULL UNIQUE
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE categoria(
-idCategoria INT NOT NULL PRIMARY KEY,
-nobreCategoria VARCHAR(30)
-);
 
-CREATE TABLE sancion(
-idSancion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-fechaInicioSancion DATETIME NOT NULL,
-fechaFinSancion DATETIME NOT NULL,
-idCliente_fk INT,
- FOREIGN KEY (idCliente_fk) REFERENCES cliente(idCliente)
-);
-CREATE TABLE articulo(
-idArticulo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nobreArticulo VARCHAR(70) NOT NULL,
-marcaArticulo VARCHAR(40),
-descripcionArticulo VARCHAR(200),
-valorArticulo DOUBLE,
-idCategoria_fk INT,
-idCliente_fk INT,
- FOREIGN KEY (idCategoria_fk) REFERENCES categoria(idCategoria),
-FOREIGN KEY (idCliente_fk) REFERENCES cliente(idCliente)
-);
-CREATE TABLE remate(
-idRemate INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-fechaInicio DATETIME,
-fechaCierre DATETIME,
-valorInicial DOUBLE,
-idArticulo_fk INT,
-FOREIGN KEY (idArticulo_fk) REFERENCES articulo(idArticulo)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE puja(
-idPuja INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-valorMayor DOUBLE NOT NULL,
-fechaOferta DATETIME NOT NULL,
-idRemate_fk INT, 
-idClienteOfertante_fk INT, 
-FOREIGN KEY (idRemate_fk) REFERENCES remate(idRemate),
-FOREIGN KEY (idClienteOfertante_fk) REFERENCES  cliente(idCliente)
-);
+--
+-- Base de datos: `bdremates`
+--
 
-CREATE TABLE venta(
-idVenta INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-valorVenta DOUBLE NOT NULL,
-fechaVenta DATETIME NOT NULL,
-fechaLimite DATETIME NOT NULL,
-clienteComprador_fk INT,
-clienteVendedor_fk INT, 
-FOREIGN KEY (clienteComprador_fk) REFERENCES cliente(idCliente),
-FOREIGN KEY (clienteVendedor_fk) REFERENCES cliente(idCliente)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE pago(
-idPago INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-fechaPago DATETIME NOT NULL,
-valorPago DOUBLE NOT NULL,
-idVenta_fk INT,
-FOREIGN KEY (idVenta_fk) REFERENCES venta(idVenta) 
-)
+--
+-- Estructura de tabla para la tabla `articulo`
+--
+
+DROP TABLE IF EXISTS `articulo`;
+CREATE TABLE IF NOT EXISTS `articulo` (
+  `idArticulo` int(11) NOT NULL AUTO_INCREMENT,
+  `nobreArticulo` varchar(70) NOT NULL,
+  `marcaArticulo` varchar(40) DEFAULT NULL,
+  `descripcionArticulo` varchar(200) DEFAULT NULL,
+  `valorArticulo` double DEFAULT NULL,
+  `Foto` varchar(50) DEFAULT NULL,
+  `idCategoria_fk` int(11) DEFAULT NULL,
+  `idCliente_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idArticulo`),
+  KEY `idCategoria_fk` (`idCategoria_fk`),
+  KEY `idCliente_fk` (`idCliente_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nobreCategoria` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idCategoria`, `nobreCategoria`) VALUES
+(1, 'Electrodoméstico'),
+(2, 'Calzado'),
+(3, 'Ropa'),
+(4, 'Tecnología'),
+(5, 'Mueble'),
+(6, 'Decoración '),
+(7, 'Implementos de cocina '),
+(8, 'Herramientas '),
+(9, 'Juguetes'),
+(10, 'Vehículo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreCliente` varchar(60) NOT NULL,
+  `apellidoCliente` varchar(60) NOT NULL,
+  `cedulaCliente` varchar(12) NOT NULL,
+  `contrasenaCliente` varchar(20) NOT NULL,
+  `nombreUsuario` varchar(40) NOT NULL,
+  `nivelAcceso` tinyint(4) NOT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `nombreUsuario` (`nombreUsuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `nombreCliente`, `apellidoCliente`, `cedulaCliente`, `contrasenaCliente`, `nombreUsuario`, `nivelAcceso`) VALUES
+(1, 'Administrador', 'Admin', '123456', '123456', 'Admin@email.com', 1),
+(3, 'carlos', 'Perez', '123456789', '147852', 'caperez@correo.com', 0),
+(12, 'usuario', 'dos', '456', '123', '456@correo.com', 0),
+(5, 'usuario', 'uno', '1245', '123456', '123@correo.com', 0),
+(10, 'Luis', 'Ortiz', '4562147', '147', '1234@correo.com', 1),
+(11, 'admin1', 'Uno', '963', '258', '258@correo.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+DROP TABLE IF EXISTS `pago`;
+CREATE TABLE IF NOT EXISTS `pago` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaPago` datetime NOT NULL,
+  `valorPago` double NOT NULL,
+  `idVenta_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPago`),
+  KEY `idVenta_fk` (`idVenta_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puja`
+--
+
+DROP TABLE IF EXISTS `puja`;
+CREATE TABLE IF NOT EXISTS `puja` (
+  `idPuja` int(11) NOT NULL AUTO_INCREMENT,
+  `valorMayor` double NOT NULL,
+  `fechaOferta` datetime NOT NULL,
+  `idRemate_fk` int(11) DEFAULT NULL,
+  `idClienteOfertante_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPuja`),
+  KEY `idRemate_fk` (`idRemate_fk`),
+  KEY `idClienteOfertante_fk` (`idClienteOfertante_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `remate`
+--
+
+DROP TABLE IF EXISTS `remate`;
+CREATE TABLE IF NOT EXISTS `remate` (
+  `idRemate` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaInicio` datetime DEFAULT NULL,
+  `fechaCierre` datetime DEFAULT NULL,
+  `valorInicial` double DEFAULT NULL,
+  `idArticulo_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idRemate`),
+  KEY `idArticulo_fk` (`idArticulo_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sancion`
+--
+
+DROP TABLE IF EXISTS `sancion`;
+CREATE TABLE IF NOT EXISTS `sancion` (
+  `idSancion` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaInicioSancion` datetime NOT NULL,
+  `fechaFinSancion` datetime NOT NULL,
+  `idCliente_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idSancion`),
+  KEY `idCliente_fk` (`idCliente_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE IF NOT EXISTS `venta` (
+  `idVenta` int(11) NOT NULL AUTO_INCREMENT,
+  `valorVenta` double NOT NULL,
+  `fechaVenta` datetime NOT NULL,
+  `fechaLimite` datetime NOT NULL,
+  `clienteComprador_fk` int(11) DEFAULT NULL,
+  `clienteVendedor_fk` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idVenta`),
+  KEY `clienteComprador_fk` (`clienteComprador_fk`),
+  KEY `clienteVendedor_fk` (`clienteVendedor_fk`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 
 
